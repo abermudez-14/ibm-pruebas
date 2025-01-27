d6ea166fe1753786c6135dced57272e38931ea27
abermudez-14 <abermudez14@stemdo.io>
2025-01-27 12:22:53 +0100

    Pruebas-Ejer6

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    ibm = {
+      source = "IBM-Cloud/ibm"
+      version = ">= 1.12.0"
+    }
+  }
+}
+
 provider "ibm" {
   ibmcloud_api_key = var.api_key
   region           = "eu-es"
