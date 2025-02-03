625a1450a102395597fe7cf796d28fa7e6547ed3
abermudez-14 <abermudez14@stemdo.io>
2025-02-03 09:56:44 +0100

    cambio

--- main.tf
+++ main.tf
@@ -8,117 +8,71 @@ terraform {
 }
 
 provider "ibm" {
+  region = "eu-es"
   ibmcloud_api_key = var.api_key
-  region           = "eu-es"
 }
 
-# Crear una VPC
-resource "ibm_is_vpc" "vpc_abermudez" {
-  name = "vpc-abermudez"
+# VPC (reutilizada del ejercicio 5)
+resource "ibm_is_vpc" "vpc_module_rgonzalez" {
+  name = "vpc-rgonzalez"
   resource_group = var.resource_group
 }
 
-# Crear dos subredes en la VPC
-resource "ibm_is_subnet" "subnet1" {
-  name                     = "subnet1"
-  vpc                      = ibm_is_vpc.vpc_abermudez.id
-  zone                     = "eu-es-1"
-  total_ipv4_address_count = 256
+# Subredes en dos zonas distintas (modificado para ejercicio 6)
+resource "ibm_is_subnet" "subnet_1" {
+  name            = "subnet-rgonzalez-zona1"
+  vpc             = ibm_is_vpc.vpc_module_rgonzalez.id
+  zone            = "eu-es-1"
+  # ipv4_cidr_block = "10.251.10.0/24"
+  total_ipv4_address_count = 256  # Ampliado para permitir más direcciones
+  resource_group  = var.resource_group
+  # network_acl = ibm_is_network_acl.acl.id 
+}
+
+resource "ibm_is_subnet" "subnet_2" {
+  name            = "subnet-rgonzalez-zona2"
+  vpc             = ibm_is_vpc.vpc_module_rgonzalez.id
+  zone            = "eu-es-2"  # Nueva zona
+  # ipv4_cidr_block = "10.251.20.0/24"  # Nuevo rango
+  total_ipv4_address_count = 256  # Ampliado para permitir más direcciones
+  resource_group  = var.resource_group
+}
+
+# Puertas de enlace públicas (nuevo para ejercicio 6)
+resource "ibm_is_public_gateway" "pgw_1" {
+  name           = "pgw-zona1"
+  vpc            = ibm_is_vpc.vpc_module_rgonzalez.id
   resource_group = var.resource_group
-    
+  zone           = "eu-es-1"
 }
 
-resource "ibm_is_subnet" "subnet2" {
-  name                     = "subnet2"
-  vpc                      = ibm_is_vpc.vpc_abermudez.id
-  zone                     = "eu-es-2"
-  total_ipv4_address_count = 256
-  resource_group = var.resource_group  
-}
-
-# Crear dos instancias (VMs) en las subredes
-resource "ibm_is_instance" "vm1" {
-  name    = "vm1-abermudez"
-  vpc     = ibm_is_vpc.vpc_abermudez.id
-  zone    = "eu-es-1"
-  keys    = [ibm_is_ssh_key.ssh_key.id]
-  image   = "r050-b98611da-e7d8-44db-8c42-2795071eec24" # ID de una imagen de Ubuntu
-  profile = "bx2-2x8"
+resource "ibm_is_public_gateway" "pgw_2" {
+  name           = "pgw-zona2"
+  vpc            = ibm_is_vpc.vpc_module_rgonzalez.id
   resource_group = var.resource_group
-
-  primary_network_interface {
-    subnet = ibm_is_subnet.subnet1.id
-    security_groups = [ibm_is_security_group.ssh_abermudez_security_group.id]
-
-  }
+  zone           = "eu-es-2"
 }
 
-resource "ibm_is_instance" "vm2" {
-  name    = "vm2-abermudez"
-  vpc     = ibm_is_vpc.vpc_abermudez.id
-  zone    = "eu-es-2"
-  keys    = [ibm_is_ssh_key.ssh_key.id]
-  image   = "r050-b98611da-e7d8-44db-8c42-2795071eec24" # ID de una imagen de Ubuntu
-  profile = "bx2-2x8"
-  resource_group = var.resource_group  
-
-  primary_network_interface {
-    subnet = ibm_is_subnet.subnet2.id
-    security_groups = [ibm_is_security_group.ssh_abermudez_security_group.id]
-  }
+# Asociación de puertas de enlace a subredes
+resource "ibm_is_subnet_public_gateway_attachment" "pg_attach1" {
+  subnet         = ibm_is_subnet.subnet_1.id
+  public_gateway = ibm_is_public_gateway.pgw_1.id
 }
 
-# Crear un balanceador de carga
-resource "ibm_is_lb" "load_balancer" {
-  name    = "lb-abermudez"
-  subnets = [ibm_is_subnet.subnet1.id, ibm_is_subnet.subnet2.id]
-  type    = "public"
+resource "ibm_is_subnet_public_gateway_attachment" "pg_attach2" {
+  subnet         = ibm_is_subnet.subnet_2.id
+  public_gateway = ibm_is_public_gateway.pgw_2.id
+}
+
+# Security Group (ampliado para permitir HTTP)
+resource "ibm_is_security_group" "sg_web" {
+  name           = "sg-web-rgonzalez"
+  vpc            = ibm_is_vpc.vpc_module_rgonzalez.id
   resource_group = var.resource_group
-  security_groups = [ ibm_is_security_group.ssh_abermudez_security_group.id]
 }
 
-# Crear un pool de backend para el balanceador de carga
-resource "ibm_is_lb_pool" "backend_pool" {
-  lb               = ibm_is_lb.load_balancer.id
-  name             = "backend-pool"
-  protocol         = "http"
-  algorithm        = "round_robin"
-  health_delay     = 5
-  health_retries   = 2
-  health_timeout   = 2
-  health_type      = "http"
-  health_monitor_url = "/"
-  
-}
-
-# Agregar las instancias al pool de backend
-resource "ibm_is_lb_pool_member" "member1" {
-  lb       = ibm_is_lb.load_balancer.id
-  pool     = ibm_is_lb_pool.backend_pool.id
-  port     = 80
-  target_address   = ibm_is_instance.vm1.primary_network_interface[0].primary_ipv4_address
-  
-}
-
-resource "ibm_is_lb_pool_member" "member2" {
-  lb       = ibm_is_lb.load_balancer.id
-  pool     = ibm_is_lb_pool.backend_pool.id
-  port     = 80
-  target_address = ibm_is_instance.vm2.primary_network_interface[0].primary_ipv4_address
-}
-
-
-
-resource "ibm_is_security_group" "ssh_abermudez_security_group" {
-  name            = "ssh-security-group"
-  vpc          =  ibm_is_vpc.vpc_abermudez.id
-  resource_group  = var.resource_group  
-}
-
-
-
-resource "ibm_is_security_group_rule" "ssh_rule" {
-  group     = ibm_is_security_group.ssh_abermudez_security_group.id
+resource "ibm_is_security_group_rule" "ssh" {
+  group     = ibm_is_security_group.sg_web.id
   direction = "inbound"
   remote    = "0.0.0.0/0"
   tcp {
@@ -127,9 +81,8 @@ resource "ibm_is_security_group_rule" "ssh_rule" {
   }
 }
 
-
-resource "ibm_is_security_group_rule" "http_rule" {
-  group     = ibm_is_security_group.ssh_abermudez_security_group.id
+resource "ibm_is_security_group_rule" "http" {
+  group     = ibm_is_security_group.sg_web.id
   direction = "inbound"
   remote    = "0.0.0.0/0"
   tcp {
@@ -138,38 +91,141 @@ resource "ibm_is_security_group_rule" "http_rule" {
   }
 }
 
-resource "ibm_is_security_group_rule" "internet_rule" {
-  group     = ibm_is_security_group.ssh_abermudez_security_group.id
+# Agregar estas reglas al security group
+resource "ibm_is_security_group_rule" "outbound_all" {
+  group     = ibm_is_security_group.sg_web.id
   direction = "outbound"
   remote    = "0.0.0.0/0"
-
 }
 
+resource "ibm_is_security_group_rule" "icmp" {
+  group     = ibm_is_security_group.sg_web.id
+  direction = "inbound"
+  remote    = "0.0.0.0/0"
+  icmp {
+    type = 8
+  }
+}
 
-
-resource "ibm_is_ssh_key" "ssh_key" {
-  name       = "ssh-key-abermudez"
+# SSH Key (reutilizada del ejercicio 5)
+resource "ibm_is_ssh_key" "ssh_key_rgonzalez" {
+  name       = "ssh-key-rgonzalez"
   public_key = <<-EOF
- ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDQYePYr1IxSOGxJ6+lKuD4onsLK8jxU93BvYAB2lxTgomteXCpdHnKK3jix8hxadmANkG/k9kEjxWwKQR7ZVyw8eQul3aLCfMnHGqplVQH3JSsz5bKMaCNx8r2P5SYGLeTmbixZUmjlFxeacEQ7/8RPvVESZ5IvrOpNtsW0kF3IsxXZndLhZlC+a69xIw2UTDVYRjwSFcB4BLl2Z3YPIwcFNWyDQdThmSWJkfdXxOmunaVRVK+OFhEAJmIf8TJ6JVBbsBf1RU2khD8M3zGpxTKF6W0rb9seEkfHERhJbYpv8NmyWST8vgyCYRElKQK+IWmT4qMua+q6eXcrUtalyZa1m8rIytze10sa4kBsN/fdr/rtACDo+hx/e1lU5GnwodPscFaVHHH5nIOF1iq4llRevoPsTvSwViAE9Se1BrLZC1MrpyxF8l7LTDqCYbRuWoTXP5w5ElbqKIEbaBvv3xhd8V7jW0VYvg/vSbD9ZApAmb7QRnzzjGLCKS9k5/rOvhtcT/FP7XXxivnc+tRp7Q+FRjHAPgmhd9unk/LTUjXhaD9+M30nDol39jT+jwBZ8JOW1rFEFQJkGM7wfqSzbJRQutH5VMCX3XSk1+qv2hz5Sza1IJJPfeleetFRT9b1AbU/TCRpOg7ZwrcvMd9xyWFacHTqaUR2/oXF2c6FzT6FQ== abermudez@stemdo
+  ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCU94A3wzNYKAAYrOgQ6OGPcLVNYb73+FF5r/Vp/upSghDbdRzW95xm4BBTqaR+8Dm81UFycPjJlYnUaKYlrjGpTxKLoX6myC/RA0ddYH9WAD6ZRqdXepELdoikiZyvMOaMgOT5t6t9z9tWCuzkgvc5L8goYfHXzP44iGrkqR3Vf0Q3PmnHedFFFShbcT3p1vKR/9Z7VFF2my0Weg0C7tpE7VRBQ1dFlhzKCbAhWQ9SqZUowlh7/ASGzgX9K9czV6MtvE932YudPlSKrpD1GRejY+sndAfl1yOObyvKkUXmMjoqWIsRV3QBJtTNJNQk09MHMmwNEvTlW7T+ffe3Asqz
   EOF
   resource_group = var.resource_group
-  
 }
 
+# Máquinas virtuales en ambas zonas
+resource "ibm_is_instance" "vm1" {
+  name           = "vm-rgonzalez-zona1"
+  vpc            = ibm_is_vpc.vpc_module_rgonzalez.id
+  profile        = "bx2-2x8"
+  zone           = "eu-es-1"
+  keys           = [ibm_is_ssh_key.ssh_key_rgonzalez.id]
+  image          = "r050-b98611da-e7d8-44db-8c42-2795071eec24"
+  resource_group = var.resource_group
 
-resource "ibm_is_floating_ip" "public_ip1" {
-  name   = "public-ip1-abermudez"
+  primary_network_interface {
+    subnet          = ibm_is_subnet.subnet_1.id
+    security_groups = [ibm_is_security_group.sg_web.id]
+  }
+}
+
+resource "ibm_is_instance" "vm2" {
+  name           = "vm-rgonzalez-zona2"
+  vpc            = ibm_is_vpc.vpc_module_rgonzalez.id
+  profile        = "bx2-2x8"
+  zone           = "eu-es-2"
+  keys           = [ibm_is_ssh_key.ssh_key_rgonzalez.id]
+  image          = "r050-b98611da-e7d8-44db-8c42-2795071eec24"
+  resource_group = var.resource_group
+
+  primary_network_interface {
+    subnet          = ibm_is_subnet.subnet_2.id
+    security_groups = [ibm_is_security_group.sg_web.id]
+  }
+}
+
+# IPs públicas para ambas VMs
+resource "ibm_is_floating_ip" "ip_vm1" {
+  name   = "ip-vm1-rgonzalez"
   target = ibm_is_instance.vm1.primary_network_interface[0].id
   resource_group = var.resource_group
-  depends_on = [ibm_is_instance.vm1]
+}
 
+resource "ibm_is_floating_ip" "ip_vm2" {
+  name   = "ip-vm2-rgonzalez"
+  target = ibm_is_instance.vm2.primary_network_interface[0].id
+  resource_group = var.resource_group
+}
+
+# Balanceador de carga (nuevo para ejercicio 6)
+resource "ibm_is_lb" "lb_web" {
+  name           = "lb-web-rgonzalez"
+  type           = "public"
+  subnets        = [ibm_is_subnet.subnet_1.id, ibm_is_subnet.subnet_2.id]
+  security_groups = [ibm_is_security_group.sg_web.id]
+  resource_group = var.resource_group
+}
+
+resource "ibm_is_lb_pool" "pool_web" {
+  name           = "pool-web-rgonzalez"
+  lb             = ibm_is_lb.lb_web.id
+  algorithm      = "round_robin"
+  protocol       = "http"
+  health_delay   = 5
+  health_retries = 2
+  health_timeout = 2
+  health_type = "http"
+}
+
+resource "ibm_is_lb_listener" "http" {
+  lb           = ibm_is_lb.lb_web.id
+  port         = 80
+  protocol     = "http"
+  default_pool = ibm_is_lb_pool.pool_web.pool_id
+}
+
+resource "ibm_is_lb_pool_member" "member1" {
+  lb             = ibm_is_lb.lb_web.id
+  pool           = ibm_is_lb_pool.pool_web.pool_id
+  port           = 80
+  target_address = ibm_is_instance.vm1.primary_network_interface[0].primary_ipv4_address
+}
+
+resource "ibm_is_lb_pool_member" "member2" {
+  lb             = ibm_is_lb.lb_web.id
+  pool           = ibm_is_lb_pool.pool_web.pool_id
+  port           = 80
+  target_address = ibm_is_instance.vm2.primary_network_interface[0].primary_ipv4_address
+}
+
+resource "ibm_resource_instance" "cos_instance" {
+  name              = "cos-abermudez"
+  service          = "cloud-object-storage"
+  plan             = "standard"
+  location         = "eu-es"
+  resource_group_id = var.resource_group
+}
+
+resource "ibm_cos_bucket" "static_bucket" {
+  bucket_name       = "mi-web-estatica-bucket"
+  resource_instance_id = ibm_resource_instance.cos_instance.id
+  storage_class     = "standard"
 }
 
 
-resource "ibm_is_floating_ip" "public_ip2" {
-  name   = "public-ip2-abermudez"
-  target = ibm_is_instance.vm2.primary_network_interface[0].id
-  resource_group = var.resource_group
-  depends_on = [ibm_is_instance.vm2]
 
+# Outputs para acceso
+output "ip_balanceador" {
+  value = ibm_is_lb.lb_web.hostname
+}
+
+output "ip_publica_vm1" {
+  value = ibm_is_floating_ip.ip_vm1.address
+}
+
+output "ip_publica_vm2" {
+  value = ibm_is_floating_ip.ip_vm2.address
 }
