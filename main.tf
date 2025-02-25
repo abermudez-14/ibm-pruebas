4408af9bbfcb4e443b692d12c93a726fde14a8c7
abermudez-14 <abermudez14@stemdo.io>
2025-02-25 16:47:19 +0100

    Added Kubernetes provider

--- main.tf
+++ main.tf
@@ -12,14 +12,29 @@ provider "ibm" {
   ibmcloud_api_key = var.api_key
 }
 
+provider "kubernetes" {
+  config_context_cluster = "ez-ibm-openshift-vpc"
+}
 
-resource "ibm_iam_user_invite" "wiki_user" {
-  users = ["hgonzalez@stemdo.io"]
-  iam_policy {
-    roles = ["Viewer", "Editor"]
-    resources {
-      service           = "containers-kubernetes"
-      resource_group_id = var.resource_group
-    }
+# resource "ibm_iam_user_invite" "wiki_user" {
+#   users = ["pruebasLABS@stemdo.io"]
+#   iam_policy {
+#     roles = ["Viewer", "Editor"]
+#     resources {
+#       service           = "containers-kubernetes"
+#       resource_group_id = var.resource_group
+#     }
+#   }
+# }
+
+resource "ibm_iam_access_group" "stemdowiki" {
+ name        = "stemdo_WIKI"
+ description = "New access group"
+}
+
+resource "kubernetes_namespace" "stemdo-wiki" {
+  metadata {
+    name = "stemdo-wiki"
+
   }
 }
