9a1cb053b961b3de73583b5147980af6a2b3d009
abermudez-14 <abermudez14@stemdo.io>
2025-02-26 11:34:31 +0100

    Added access group for Wiki

--- main.tf
+++ main.tf
@@ -31,24 +31,15 @@ data "ibm_container_cluster_config" "cluster_config" {
   admin             = true
 }
 
-# resource "ibm_iam_user_invite" "wiki_user" {
-#   users = ["pruebasLABS@stemdo.io"]
-#   iam_policy {
-#     roles = ["Viewer", "Editor"]
-#     resources {
-#       service           = "containers-kubernetes"
-#       resource_group_id = var.resource_group
-#     }
-#   }
-# }
 
-# resource "ibm_iam_access_group" "stemdowiki" {
-#  name        = "stemdo_WIKI"
-#  description = "New access group"
-# }
+resource "ibm_iam_access_group" "stemdowiki" {
+ name        = "stemdo_WIKI"
+ description = "New access group"
+}
 
 resource "kubernetes_namespace" "stemdo-wiki" {
   metadata {
     name = "stemdo-wiki"
   }
 }
+
